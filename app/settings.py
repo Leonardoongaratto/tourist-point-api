7d8028868c5054d99f8bbbad658228b20b78f06e
Leonardoongaratto <leonardocongarattodev@gmail.com>
2022-06-07 01:19:50 -0300

    Criado o app comments

--- app/settings.py
+++ app/settings.py
@@ -38,7 +38,8 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'core',
-    'attractions'
+    'attractions',
+    'comments',
 ]
 
 MIDDLEWARE = [
---
+++ comments/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+from django.contrib.auth.models import User
+
+class Comentario(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    comment = models.TextField()
+    date = models.DateTimeField(auto_now_add=True)
+    approved = models.BooleanField(default=True)
