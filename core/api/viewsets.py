8017c65e74f3aa3635529c79b0acbc8adf9b5d06
Leonardoongaratto <leonardocongarattodev@gmail.com>
2022-06-09 19:54:07 -0300

    Sobrescrevendo as actions destoy, retrieve, update, partial_update

--- core/api/viewsets.py
+++ core/api/viewsets.py
@@ -14,3 +14,15 @@ class PontoTuristicoViewSet(viewsets.ModelViewSet):
     
     def create(self, request, *args, **kwargs):
         return super().create(request, *args, **kwargs)
+
+    def destroy(self, request, *args, **kwargs):
+        pass
+
+    def retrieve(self, request, *args, **kwargs):
+        pass
+
+    def update(self, request, *args, **kwargs):
+        pass
+
+    def partial_update(self, request, *args, **kwargs):
+        pass
