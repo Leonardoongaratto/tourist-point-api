d80de8b48b22bbcd46e22a0ec4ff57ad3438e612
Leonardoongaratto <leonardocongaratto1@gmail.com>
2022-06-09 16:21:10 -0300

    Feita as devidas correções

--- evaluation/apps.py
+++ evaluation/apps.py
@@ -1,6 +1,6 @@
 from django.apps import AppConfig
 
 
-class AssessmentsConfig(AppConfig):
+class EvaluationConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
-    name = 'assessments'
+    name = 'evaluation'
