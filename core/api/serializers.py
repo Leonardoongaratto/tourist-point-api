8d1960bdf2f81c71dd6268c32d2dc0f98be0e89d
Leonardoongaratto <leonardocongaratto1@gmail.com>
2022-06-15 16:41:48 -0300

    Incrementando um objeto com nested serializer

--- core/api/serializers.py
+++ core/api/serializers.py
@@ -1,8 +1,20 @@
 from core.models import PontoTuristico
 from rest_framework import serializers
+from attractions.api.serializers import AtracaoSerializer
+from comments.api.serializers import ComentarioSerializer
+from evaluation.api.serializers import AvaliacaoSerializer
+from address.api.serializers import EnderecoSerializer
 
 
 class PontoTuristicoSerializer(serializers.HyperlinkedModelSerializer):
+    attraction = AtracaoSerializer(many=True, read_only=True)
+    comment = ComentarioSerializer(many=True, read_only=True)
+    evaluation = AvaliacaoSerializer(many=True, read_only=True)
+    address = EnderecoSerializer()
+    
     class Meta:
         model = PontoTuristico
-        fields = ['id', 'name', 'description', 'picture']
+        fields = [
+            'id', 'name', 'description', 'picture',
+            'attraction','comment', 'evaluation', 'address'
+        ]
